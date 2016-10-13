ef5588ee405bf7740e7914df311cd29a00df3d31
Yuuki TANAAMI <st13d26@gm.ibaraki-ct.ac.jp>
2016-10-13 15:27:24 +0900

    main.c

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 int main(void)
 {
 	int payments[5];
-	int i;
-	for (i = 0; i < 5; i++) {
-		scanf("%d", &payments[i]);
+	int j;
+	for (j = 0; j < 5; j++) {
+		scanf("%d", &payments[j]);
 	}
 
 	return 0;
